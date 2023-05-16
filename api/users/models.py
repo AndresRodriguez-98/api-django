a403d380ab47d7e12865f59e337706bfd75bb6e6
Andrés Rodriguez <andresrodrigueza@hotmail.com>
2023-05-16 19:30:11 -0300

    WIP creacion-de-modelos-de-datos-red-social

--- api/users/models.py
+++ api/users/models.py
@@ -12,6 +12,15 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class UsuarioSocial(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    nick_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=100)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
